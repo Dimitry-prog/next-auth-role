6c029c985b7134ce954ba294152ed67a1a91898f
Dmitry <kfifa3@gmail.com>
2024-02-11 00:39:54 +0300

    feat: db

--- features/auth/actions/index.tsx
+++ features/auth/actions/index.tsx
@@ -2,6 +2,9 @@
 
 import { LoginFormType } from '@/features/auth/types';
 import { loginSchema, registerSchema } from '@/features/auth/validation';
+import { getUserByEmail } from '@/services/user';
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
 
 export const login = async (data: LoginFormType) => {
   const validatedFields = loginSchema.safeParse(data);
@@ -26,7 +29,34 @@ export const register = async (data: LoginFormType) => {
     };
   }
 
-  return {
-    success: 'Ok',
-  };
+  const { email, name, password } = validatedFields.data;
+
+  try {
+    const isUserExist = await getUserByEmail(email);
+
+    if (isUserExist) {
+      return {
+        error: 'Email already in use!',
+      };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      success: 'User created',
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      error: 'Database Error: Failed to create user.',
+    };
+  }
 };
